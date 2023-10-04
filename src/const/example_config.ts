fa1664975b6bd80a95e0baea18960a58801c7662
Rapha01 <sebashatt@gmail.com>
2023-10-04 06:53:59 +0200

    Genesis.

---
+++ src/const/example_config.ts
@@ -0,0 +1,9 @@
+interface Config {
+    env: string;
+}
+   
+const c: Config = {
+    env: 'dev'
+}
+
+module.exports = config;
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+
+const config = require('./const/config.ts');
+console.log(config.env);
+
+interface User {
+    name: string;
+    id: number;
+}
+
+class UserAccount {
+    name: string;
+    id: number;
+   
+    constructor(name: string, id: number) {
+      this.name = name;
+      this.id = id;
+    }
+}
+   
+const user: User = new UserAccount("Murphy", 1);
+
+console.log(user);
